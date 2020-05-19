86c5f25b281f939e63194daef82f3778d62c0a49
King Li <king@qupital.com>
2020-05-19 23:24:20 +0800

    Description and readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 eaturl
+
+This is a urlshortener written in golang, deployable in AWS via Terraform
+
+## How to build
+
+We utilize packer as image packaging tool here, internally we use shell script, golang utils to build the software and manage dependencies.
+
+```sh
+packer validate packer.json
+packer build packer.json
+```
+
+## How to deploy
+
+Supply to required to variables specified in variables.tf, then
+
+```sh
+terraform plan
+terraform apply
+```
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,42 +1,51 @@
 variable "region" {
-  type    = string
-  default = "ap-southeast-1"
+  type        = string
+  default     = "ap-southeast-1"
+  description = "Region this stack resides in"
 }
 
 variable "access_key" {
-  type = string
+  type        = string
+  description = "Access key for amazon services"
 }
 
 variable "secret_key" {
-  type = string
+  type        = string
+  description = "Secret key for amazon services"
 }
 
 variable "number_of_zone" {
-  type    = number
-  default = 2
+  type        = number
+  default     = 2
+  description = "Number of az does the deployment span"
 }
 
 variable "prefix" {
-  type    = string
-  default = "alpha"
+  type        = string
+  default     = "alpha"
+  description = "Prefix for resources deployed"
 }
 
 variable "host_zone" {
   type = string
   # default = "base.blackcastle.pw"
+  description = "Host zone dns record"
 }
 
 variable "sub_domain" {
   type = string
   # default = "happy"
+  description = "sub domain for dns record"
 }
 
 variable "acm_id" {
   type = string
   # default = "arn:aws:acm:ap-southeast-1:xxxxxxx:certificate/xxxxxxxxx"
+  description = "arn for acm certificate"
 }
 
 variable "ami" {
   type = string
   # default = "ami-xxxxxxxx"
+  description = "AMI for image generated with packer"
 }
